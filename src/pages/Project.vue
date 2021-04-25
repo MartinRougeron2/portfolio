c6be3ab28468b7c6e94faf16ceab447af58ca1ba
Martin Rougeron <martin.rougeron@epitech.eu>
2021-04-25 14:09:15 +0200

    fix for phones

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -4,15 +4,16 @@
       <q-img
           class="rounded-borders col-10 full-height row shadow-10"
           :src="project.image_url"
-          style="max-width: 50vw; transition: 0.8s"
+          style="max-width: 70vw; transition: 0.8s"
           :ratio="2"
       >
+        <div class="flex flex-center basic-text col-9 text-white absolute-bottom " style="font-size: min(50px, 3vw);">
+          Project {{ project.name }}
+        </div>
       </q-img>
-      <div class="flex flex-center basic-text col-12 basic-text text-white" style="font-size: 50px; transform: translateY(-150px)">
-        Project {{ project.name }}
-      </div>
     </div>
-    <div class="row justify-start q-pl-xl q-pa-md text-h4">
+    <div class="xs-hide sm-hide">
+      <div class="row justify-start q-pl-xl-xl q-pa-md" style="font-size: min(20px, 4vw)">
         <div class="col-7 text-justify">
           <div class="q-pa-md">
             {{ project.text1 }}
@@ -21,14 +22,37 @@
         <div class="flex flex-center q-ml-xl text-h2">
           Project Goal
         </div>
-    </div>
-    <div class="row justify-end q-pl-xl q-pa-md text-h4">
-      <div class="flex flex-center q-mr-xl text-h2">
-        Achievements
       </div>
-      <div class="col-7 text-justify">
-        <div class="q-pa-md">
-          {{ project.text2 }}
+      <div class="row justify-end q-pl-xl-xl q-pa-md" style="font-size: min(20px, 4vw)">
+        <div class="flex flex-center q-mr-xl text-h2">
+          Achievements
+        </div>
+        <div class="col-7 text-justify">
+          <div class="q-pa-md">
+            {{ project.text2 }}
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="xs-show sm-show">
+      <div class="row justify-center q-pl-xl-xl q-pa-md" style="font-size: min(20px, 4vw)">
+        <div class="flex flex-center col-10 q-mt-lg text-h3">
+          Project Goal
+        </div>
+        <div class="col-10 text-justify">
+          <div class="q-pa-md">
+            {{ project.text1 }}
+          </div>
+        </div>
+      </div>
+      <div class="row justify-center q-pl-xl-xl q-pa-md" style="font-size: min(20px, 4vw)">
+        <div class="flex flex-center col-10 q-mt-lg text-h3">
+          Achievements
+        </div>
+        <div class="col-10 text-justify">
+          <div class="q-pa-md">
+            {{ project.text2 }}
+          </div>
         </div>
       </div>
     </div>
